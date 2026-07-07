bf3859c7678934a0e15611e8194fcc81cb86210c
Dominik Ocsofszki <dominikocsofszki@gmail.com>
2026-07-07 20:07:58 +0200

    change

--- apps/x2/src/test/java/parkhouse/calculations/LocatorTest.java
+++ apps/x2/src/test/java/parkhouse/calculations/LocatorTest.java
@@ -36,7 +36,7 @@ public class LocatorTest {
     }
 
     @Test
-    @DisplayName("Test if correct space is assigned to entering cars")
+    @DisplayName("Test if correct space is assigned to entering cars22")
     public void locatorLocateTest() {
         for (ICar c : cars) {
         // for (ICar c : cars) {
